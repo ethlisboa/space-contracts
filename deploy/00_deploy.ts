df1cc3675e7de581bd8ca0b0acbecef14a4f35f5
Marin <marin.petrunic@gmail.com>
2021-10-24 01:42:13 +0100

    deploy update

--- deploy/00_deploy.ts
+++ deploy/00_deploy.ts
@@ -1,46 +1,90 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { DeployFunction } from 'hardhat-deploy/types';
+import { Celestial } from "./../typechain/Celestial.d";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
 import { DeployOptions, TxOptions } from "hardhat-deploy/dist/types";
 import { ContractTransaction } from "ethers";
 import { ItemKind, CelestialKind } from "../util/enums";
 
 async function run(txPromise: Promise<ContractTransaction>): Promise<any> {
-    const tx = await txPromise;
-    return await tx.wait();
+  const tx = await txPromise;
+  return await tx.wait();
 }
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { deployments, getNamedAccounts } = hre;
-    const { deploy, execute } = deployments;
-    const { deployer } = await getNamedAccounts();
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy, execute } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-    const txOptions: TxOptions = {
-        from: deployer,
-        autoMine: true,
-        log: true
+  const txOptions: TxOptions = {
+    from: deployer,
+    autoMine: true,
+    log: true,
+  };
+
+  function depOptions(...args: any): DeployOptions {
+    return {
+      from: deployer,
+      autoMine: true,
+      log: true,
+      args: [...args],
     };
+  }
 
-    function depOptions(...args: any): DeployOptions {
-        return {
-            from: deployer,
-            autoMine: true,
-            log: true,
-            args: [...args]
-        }
+  const Galaxy = await deploy("Galaxy", depOptions());
+  const Items = await deploy("Items", depOptions());
+  const Planet = await deploy("Planet", depOptions(Items.address));
+  const SpaceOven = await deploy("SpaceOven", depOptions(Items.address));
+
+  await execute(
+    "Items",
+    txOptions,
+    "setMinter",
+    ItemKind.TerrestrialWood,
+    Planet.address
+  );
+  await execute(
+    "Galaxy",
+    txOptions,
+    "setManager",
+    CelestialKind.Planet,
+    Planet.address
+  );
+
+  await execute(
+    "Galaxy",
+    txOptions,
+    "setManager",
+    CelestialKind.SpaceOven,
+    SpaceOven.address
+  );
+
+  // add some planets
+  const map: {
+    kind: number;
+    x: number;
+    y: number;
+  }[] = require("./galaxy.json");
+  let kinds = [];
+  let xs = [];
+  let ys = [];
+  let i = 0;
+  for (const celestial of map) {
+    kinds.push(celestial.kind);
+    xs.push(celestial.x);
+    ys.push(celestial.y);
+    if (i === 50) {
+      await execute("Galaxy", txOptions, "addCelestials", kinds, xs, ys);
+      i = 0;
+      kinds = [];
+      xs = [];
+      ys = [];
+      continue;
     }
-
-    const Galaxy = await deploy("Galaxy", depOptions());
-    const Items = await deploy("Items", depOptions());
-    const Planet = await deploy("Planet", depOptions(Items.address));
-    const SpaceOven = await deploy("SpaceOven", depOptions(Items.address));
-
-    await execute("Items", txOptions, "setMinter", ItemKind.TerrestrialWood, Planet.address);
-    await execute("Galaxy", txOptions, "setManager", CelestialKind.Planet, Planet.address);
-
-    // add some planets
-    await execute("Galaxy", txOptions, "addCelestial", CelestialKind.Planet, 5, 5);
-    await execute("Galaxy", txOptions, "addCelestial", CelestialKind.Planet, 10, 10);
-    await execute("Galaxy", txOptions, "addCelestial", CelestialKind.Planet, 15, 15);
+    i++;
+  }
+  if (kinds.length > 0) {
+    await execute("Galaxy", txOptions, "addCelestials", kinds, xs, ys);
+  }
 };
 export default func;
-func.tags = ['spaceXcalibur'];
+func.tags = ["spaceXcalibur"];
--- util/enums.ts
+++ util/enums.ts
@@ -1,16 +1,18 @@
+import { SpaceOven } from "./../typechain/SpaceOven.d";
 // These enums are copied from solidity files and must be kept in sync manually.
 // This is a fundamental limitation of typechain (because enum info is not in the ABI).
 
 // From `Items.sol`
 export enum ItemKind {
-    IronOre,
-    TerrestrialWood,
-    SpaceRaccoon
+  IronOre,
+  TerrestrialWood,
+  SpaceRaccoon,
 }
 
 // From `Galaxy.sol`
 export enum CelestialKind {
-    Planet,     // produces TerrestrialWood
-    Asteroid,   // produces IronOre
-    Moon        // produces SpaceRaccoon
+  Planet, // produces TerrestrialWood
+  Asteroid, // produces IronOre
+  Moon, // produces SpaceRaccoon
+  SpaceOven, // produces SpaceRaccoon
 }
